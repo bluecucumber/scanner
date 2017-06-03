4ac76e2ddaaad3c729c219ce81ef8bd2eb849f05
Mateusz <Mateusz@DESKTOP-IBNDUCF>
2017-06-03 19:33:47 +0200

    wyodrębnienie i nazwanie stałych

--- src/main/java/pl/edu/agh/mwo/Scanner.java
+++ src/main/java/pl/edu/agh/mwo/Scanner.java
@@ -10,14 +10,18 @@ public class Scanner {
 
 	public String get() {
 		String result = "";
+		String whiteChars = "[ \t\n]";
+		String parentheses = "[()]";
+		String commentBegin = "%";
+		String commentEnd = "\n";
 		boolean ok = false;
 		while (!ok) {
 			if (text.length() == 0) {
 				return result;
-			} else if (text.substring(0, 1).matches("[ \t\n]")) {
+			} else if (text.substring(0, 1).matches(whiteChars)) {
 				text = text.substring(1);
-			} else if (text.substring(0, 1).matches("%")) {
-				while (!text.substring(0, 1).matches("\n")) {
+			} else if (text.substring(0, 1).matches(commentBegin)) {
+				while (!text.substring(0, 1).matches(commentEnd)) {
 					text = text.substring(1);
 				}
 			} else {
@@ -25,11 +29,11 @@ public class Scanner {
 			}
 		}
 
-		if (text.length() > 0 && text.substring(0, 1).matches("[()]")) {
+		if (text.length() > 0 && text.substring(0, 1).matches(parentheses)) {
 			result = text.substring(0, 1);
 			text = text.substring(1);
 		}
-		while (text.length() > 0 && !text.substring(0, 1).matches("[ \t\n%()]")) {
+		while (text.length() > 0 && !text.substring(0, 1).matches(whiteChars) && !text.substring(0, 1).matches(parentheses)) {
 			result += text.substring(0, 1);
 			text = text.substring(1);
 		}
