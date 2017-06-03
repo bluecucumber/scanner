82471bc36c628050abb0a40198bbc223e14ba77d
Mateusz <Mateusz@DESKTOP-IBNDUCF>
2017-06-04 00:32:02 +0200

    Rozpoczęcie refaktoringu powtarzających się komend.

--- src/main/java/pl/edu/agh/mwo/Scanner.java
+++ src/main/java/pl/edu/agh/mwo/Scanner.java
@@ -7,28 +7,42 @@ public class Scanner {
 	public Scanner(String textInitialValue) {
 		textToScan = textInitialValue;
 	}
-
-	public String get() {
-		String result = "";
+	private boolean isTextEmpty() {
+		if (this.textToScan.length() == 0){ return true; }
+		return false;
+	}
+	private void checkOmmitedChars(){
 		String whiteChars = "[ \t\n]";
 		String parentheses = "[()]";
 		String commentBegin = "%";
 		String commentEnd = "\n";
-		boolean initialStringCheckOK = false;
-		while (!initialStringCheckOK) {
-			if (textToScan.length() == 0) {
-				return result;
-			} else if (textToScan.substring(0, 1).matches(whiteChars)) {
-				textToScan = textToScan.substring(1);
-			} else if (textToScan.substring(0, 1).matches(commentBegin)) {
-				while (!textToScan.substring(0, 1).matches(commentEnd)) {
-					textToScan = textToScan.substring(1);
-				}
-			} else {
-				initialStringCheckOK = true;
-			}
-		}
+		//String textToScan.substring(0, 1) = textToScan.substring(0, 1);
+		
+		while(textToScan)//zrobiæ pêtla która przejdzie wszystkie znaki
+		
+		/*while (textToScan.substring(0, 1).matches(whiteChars)) { textToScan = textToScan.substring(1);}//this.deleteOmmitedChars();}
+		if (textToScan.substring(0, 1).matches(commentBegin)) {
+			while (!textToScan.substring(0, 1).matches(commentEnd)) {
+				System.out.println(textToScan);
+				textToScan = textToScan.substring(1);}//this.deleteOmmitedChars();}
+		}*/
 
+	}
+	private void deleteOmmitedChars() {
+		// TODO Auto-generated method stub
+	}
+	
+	public String get() {
+		String result = "";
+		String whiteChars = "[ \t\n]";
+		String parentheses = "[()]";
+		
+
+		if(this.isTextEmpty()){return result;}
+		else{
+				this.checkOmmitedChars();
+		}
+			
 		if (textToScan.length() > 0 && textToScan.substring(0, 1).matches(parentheses)) {
 			result = textToScan.substring(0, 1);
 			textToScan = textToScan.substring(1);
@@ -39,4 +53,6 @@ public class Scanner {
 		}
 		return result.toLowerCase();
 	}
+
+
 }
