a0881dea94f729e479a7aaf7757f8b6c44c3c500
Mateusz <Mateusz@DESKTOP-IBNDUCF>
2017-06-04 14:03:40 +0200

    "isAWhiteChar", "isAComment", "deleteWhiteChar" and "deleteComment" methodes added

--- src/main/java/pl/edu/agh/mwo/Scanner.java
+++ src/main/java/pl/edu/agh/mwo/Scanner.java
@@ -3,6 +3,9 @@ package pl.edu.agh.mwo;
 public class Scanner {
 
 	String textToScan;
+	private final String whiteChars = "[ \t\n]";
+	private final String commentBegin = "%";
+	private final String commentEnd = "\n";	
 
 	public Scanner(String textInitialValue) {
 		textToScan = textInitialValue;
@@ -10,32 +13,32 @@ public class Scanner {
 	private boolean isTextEmpty() {
 		return (this.textToScan.length() == 0);
 	}
-	private void checkOmmitedChars(){
-		String whiteChars = "[ \t\n]";
-		String commentBegin = "%";
-		String commentEnd = "\n";		
-
-		while (textToScan.length() > 0 && textToScan.substring(0, 1).matches(whiteChars)) { 
-			textToScan = textToScan.substring(1);//this.deleteOmmitedChars();}
-			if (textToScan.length() > 0 && textToScan.substring(0, 1).matches(commentBegin)) {
-				do  {
-					textToScan = textToScan.substring(1);}//this.deleteOmmitedChars();}
-				while (!textToScan.substring(0, 1).matches(commentEnd));
-			}
+	private void deleteNotTokenChars() {
+		while(!isTextEmpty() && (isAWhiteChar() || isAComment())) {
+			while(!isTextEmpty() && isAWhiteChar()) { deleteWhiteChar(); }
+			while(!isTextEmpty() && isAComment()) { deleteComment(); }
 		}
 	}
-	private void deleteOmmitedChars() {
-		// TODO Auto-generated method stub
+	private boolean isAWhiteChar() {
+		return textToScan.substring(0, 1).matches(whiteChars);
 	}
-	
+	private boolean isAComment() {
+		return textToScan.substring(0, 1).matches(commentBegin);
+	}
+	private void deleteWhiteChar() {
+		textToScan = textToScan.substring(1);
+	}
+	private void deleteComment() {
+		do  {
+			textToScan = textToScan.substring(1);}
+		while (isTextEmpty() || !textToScan.substring(0, 1).matches(commentEnd));
+	}
 	public String get() {
 		String result = "";
-		String whiteChars = "[ \t\n]";
 		String parentheses = "[()]";
 		
 		while(!isTextEmpty()){
-			this.checkOmmitedChars();
-			
+			deleteNotTokenChars();
 			if (textToScan.length() > 0 && textToScan.substring(0, 1).matches(parentheses)) {
 				result = textToScan.substring(0, 1);
 				textToScan = textToScan.substring(1);
